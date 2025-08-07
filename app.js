37e44a8899898c2b0dac5b75543b0a0d4eb00c7d
João Vitor <vitoramones07022002@gmail.com>
2025-08-07 19:17:39 -0400

    Projeto Inicial

---
+++ app.js
@@ -0,0 +1,83 @@
+let listaDeNumerosSorteados = [];
+let numeroLimite = 10;
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+     if ('speechSynthesis' in window) {
+        let utterance = new SpeechSynthesisUtterance(texto);
+        utterance.lang = 'pt-BR'; 
+        utterance.rate = 1.2; 
+        window.speechSynthesis.speak(utterance); 
+    } else {
+        console.log("Web Speech API não suportada neste navegador.");
+    }
+}
+
+function exibirMensagemInicial() {
+    exibirTextoNaTela('h1', 'Jogo do número secreto');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+}
+
+exibirMensagemInicial();
+
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+    
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela('h1', 'Acertou!');
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
+        exibirTextoNaTela('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        if (chute > numeroSecreto) {
+            exibirTextoNaTela('p', 'O número secreto é menor');
+        } else {
+            exibirTextoNaTela('p', 'O número secreto é maior');
+        }
+        tentativas++;
+        limparCampo();
+    }
+}
+
+function gerarNumeroAleatorio() {
+  //Vamos verificar se o número escolhido já está dentro da lista
+    let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);
+    let quantidadeDeElementosNaLista = listaDeNumerosSorteados.length;
+
+
+    if (quantidadeDeElementosNaLista == numeroLimite) {
+      listaDeNumerosSorteados = [];
+    }
+
+       
+
+    //A lista já possui o número escolhido ? 
+    //INCLUDES = verifica se um elemento já está na lista[array], se estiver é true se não é false
+    if (listaDeNumerosSorteados.includes(numeroEscolhido)) {
+      return gerarNumeroAleatorio();
+    } else {
+      //PUSH = Adiciona alguma coisa no fim da lista [array]
+      listaDeNumerosSorteados.push(numeroEscolhido);
+      // Utilize o CONSOLE.LOG para checar o comportamento do cógido
+      console.log(listaDeNumerosSorteados)
+      return numeroEscolhido;
+    }
+
+}
+
+function limparCampo() {
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+
+function reiniciarJogo() {
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+    exibirMensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled', true);
+}
